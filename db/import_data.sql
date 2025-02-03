c977650ef81724419b578f67ca111e3f54b5cdd7
Andrei Belskii <mrandrewer@yandex.ru>
2025-02-03 23:51:29 +0300

    Add product_types table

--- db/import_data.sql
+++ db/import_data.sql
@@ -4,3 +4,11 @@ insert into material_types (material_type_name, failure_rate) values
 ('Тип материала 3', 0.28),
 ('Тип материала 4', 0.55),
 ('Тип материала 5', 0.34);
+
+insert into product_types (id, product_type_name, product_type_coef) values
+(1, 'Ламинат', 2.35),
+(2, 'Массивная доска', 5.15),
+(3, 'Паркетная доска', 4.34),
+(4, 'Пробковое покрытие', 1.5);
+
+select setval('product_types_id_seq', (select max(id) from product_types));
--- db/init_db.sql
+++ db/init_db.sql
@@ -1,7 +1,14 @@
-drop table material_types;
+drop table if exists material_types;
+drop table if exists product_types cascade;
 
 create table material_types (
     id serial primary key,
     material_type_name varchar(50),
     failure_rate numeric(5, 2)
+);
+
+create table product_types(
+    id serial primary key,
+    product_type_name varchar(50),
+    product_type_coef numeric(5, 2)
 );
