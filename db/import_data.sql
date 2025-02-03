36fa96ab9cb6ab272b7f00b600eb15326e25ac58
Andrei Belskii <mrandrewer@yandex.ru>
2025-02-04 00:05:13 +0300

    Add products table

--- db/import_data.sql
+++ db/import_data.sql
@@ -12,3 +12,12 @@ insert into product_types (id, product_type_name, product_type_coef) values
 (4, 'Пробковое покрытие', 1.5);
 
 select setval('product_types_id_seq', (select max(id) from product_types));
+
+insert into products (id, product_type, product_name, article, min_price) values
+(1,3,'Паркетная доска Ясень темный однополосная 14 мм','8758385',4456.90),
+(2,3,'Инженерная доска Дуб Французская елка однополосная 12 мм','8858958',7330.99),
+(3,1,'Ламинат Дуб дымчато-белый 33 класс 12 мм','7750282',1799.33),
+(4,1,'Ламинат Дуб серый 32 класс 8 мм с фаской','7028748',3890.41),
+(5,4,'Пробковое напольное клеевое покрытие 32 класс 4 мм','5012543',5450.59);
+
+select setval('products_id_seq', (select max(id) from product_types));
--- db/init_db.sql
+++ db/init_db.sql
@@ -1,4 +1,5 @@
 drop table if exists material_types;
+drop table if exists products cascade;
 drop table if exists product_types cascade;
 
 create table material_types (
@@ -11,4 +12,12 @@ create table product_types(
     id serial primary key,
     product_type_name varchar(50),
     product_type_coef numeric(5, 2)
+);
+
+create table products(
+    id serial primary key,
+    product_type int not null references product_types(id),
+    product_name varchar(100),
+    article varchar(50),
+    min_price numeric (12, 2)
 );
