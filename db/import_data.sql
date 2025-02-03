e2940a90c646e8181299704e0e69a20bab28e427
Andrei Belskii <mrandrewer@yandex.ru>
2025-02-04 00:25:17 +0300

    Add partners table

--- db/import_data.sql
+++ db/import_data.sql
@@ -20,4 +20,37 @@ insert into products (id, product_type, product_name, article, min_price) values
 (4,1,'Ламинат Дуб серый 32 класс 8 мм с фаской','7028748',3890.41),
 (5,4,'Пробковое напольное клеевое покрытие 32 класс 4 мм','5012543',5450.59);
 
-select setval('products_id_seq', (select max(id) from product_types));
+select setval('products_id_seq', (select max(id) from products));
+
+insert into partner_types (id, partner_type_name) values
+(1, 'ЗАО'),
+(2, 'ООО'),
+(3, 'ПАО'),
+(4, 'ОАО');
+
+select setval('partner_types_id_seq', (select max(id) from partner_types));
+
+insert into partners (
+    id,
+    partner_type,
+    partner_name,
+    director_last_name,
+    director_first_name,
+    director_middle_name,
+    email,
+    phone,
+    adress_index,
+    adress_region,
+    adress_city,
+    adress_street,
+    adress_building,
+    inn,
+    rating
+) values
+(1, 1, 'База Строитель', 'Иванова', 'Александра', 'Ивановна', 'aleksandraivanova@ml.ru', '493 123 45 67', '652050', 'Кемеровская область', 'город Юрга', 'ул. Лесная', '15', '2222455179', 7),
+(2, 2, 'Паркет 29', 'Петров', 'Василий', 'Петрович', 'vppetrov@vl.ru', '987 123 56 78', '164500', 'Архангельская область', 'город Северодвинск', 'ул. Строителей', '18', '3333888520', 7),
+(3, 3, 'Стройсервис', 'Соловьев', 'Андрей', 'Николаевич', 'ansolovev@st.ru', '812 223 32 00', '188910', 'Ленинградская область', 'город Приморск', 'ул. Парковая', '21', '4440391035', 7),
+(4, 4, 'Ремонт и отделка', 'Воробьева', 'Екатерина', 'Валерьевна', 'ekaterina.vorobeva@ml.ru', '444 222 33 11', '143960', 'Московская область', 'город Реутов', 'ул. Свободы', '51', '1111520857', 5),
+(5, 1, 'МонтажПро', 'Степанов', 'Степан', 'Сергеевич', 'stepanov@stepan.ru', '912 888 33 33', '309500', 'Белгородская область', 'город Старый Оскол', 'ул. Рабочая', '122', '5552431140', 0);
+
+select setval('partners_id_seq', (select max(id) from partners));
--- db/init_db.sql
+++ db/init_db.sql
@@ -1,4 +1,6 @@
 drop table if exists material_types;
+drop table if exists partners cascade;
+drop table if exists partner_types cascade;
 drop table if exists products cascade;
 drop table if exists product_types cascade;
 
@@ -20,4 +22,27 @@ create table products(
     product_name varchar(100),
     article varchar(50),
     min_price numeric (12, 2)
+);
+
+create table partner_types (
+    id serial primary key,
+    partner_type_name varchar(3)
+);
+
+create table partners (
+    id serial primary key,
+    partner_type int not null references partner_types(id),
+    partner_name varchar(250),
+    director_last_name varchar(50),
+    director_first_name varchar(50),
+    director_middle_name varchar(50),
+    email varchar(150),
+    phone varchar(15),
+    adress_index varchar(20),
+    adress_region varchar(50),
+    adress_city varchar(50),
+    adress_street varchar(50),
+    adress_building varchar(10),
+    inn varchar(12),
+    rating int
 );
